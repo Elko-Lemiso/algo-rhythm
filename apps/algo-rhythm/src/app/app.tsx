9916a6b4d54c596dbc9781c7b49de828a1d4e413
elkogerrits <elko@worksuite.nl>
2022-03-19 17:59:11 +0100

    first commit

--- apps/algo-rhythm/src/app/app.tsx
+++ apps/algo-rhythm/src/app/app.tsx
@@ -1,24 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@algo-rhythm/api-interfaces';
-
+import React, { useEffect, useState } from "react";
+import { Message } from "@algo-rhythm/api-interfaces";
+import { audioRecorder } from "../util/recording";
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
+  const [m, setMessage] = useState<Message>({ message: "" });
+  const [isRecording, setIsRecording] = useState(false);
 
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
+  const toggleRecording = () => {
+    audioRecorder
+      .start()
+      .then(() => {
+        //on success
+        console.log("Recording Audio...");
+      })
+      .catch((error) => {
+        //on error
+        //No Browser Support Error
+        if (
+          error.message.includes(
+            "mediaDevices API or getUserMedia method is not supported in this browser."
+          )
+        ) {
+          console.log("To record audio, use browsers like Chrome and Firefox.");
+        }
+      });
+  };
 
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to algo-rhythm!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Fast and Extensible Build System"
-        />
+      <div
+        style={{
+          textAlign: "center",
+          position: "absolute",
+          backgroundColor: "black",
+          height: "100vh",
+          width: "100%",
+        }}
+      >
+        <h1>Record something</h1>
+        <button
+          onClick={() => {
+            toggleRecording();
+          }}
+        >
+          RECORD
+        </button>
       </div>
       <div>{m.message}</div>
     </>
